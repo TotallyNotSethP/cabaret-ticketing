f12fa8f99b6198d4826b82111c4f744c9a0b8ce8
Seth Peace <sethevanpeace@gmail.com>
2021-04-08 11:26:14 -0700

    added instascan license, now using jinja2, now giving correct headers for /internals/get_id/:id

--- webserver.py
+++ webserver.py
@@ -1,21 +1,22 @@
 # Imports
-from flask import Flask, Response
+from flask import Flask, make_response, render_template
 
 # Make A Flask App
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 # Give The User index.html If They Go To The Homepage
 @app.route("/")
 def index():
-    with open("index.html") as f:
-        return f.read()
+    return render_template("index.html")
 
 
 # Shh! This will soon be used to get information about a ticket.
 @app.route("/internals/get_id/<int:id_>")
 def get_id(id_):
-    return Response("Not implemented yet.", status=503)
+    response = make_response("Not implemented yet.", 503)
+    response["Content-Type"] = "text/plain"
+    return response
 
 
 # Runs A Development Server If You Directly Run The Script
