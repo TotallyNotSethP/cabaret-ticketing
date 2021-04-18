a403cfda5b2c4ada10da1d6c7a267e50849d6e60
Seth Peace <sethevanpeace@gmail.com>
2021-04-17 22:56:59 -0700

    now gives all info on ticket

--- webserver.py
+++ webserver.py
@@ -18,12 +18,16 @@ def index():
 
 
 # Shh! This will soon be used to get information about a ticket.
-@app.route("/internals/get_id/<id_>")
-def get_id(id_):
+@app.route("/internals/get_ticket/<id_>")
+def get_ticket(id_):
     DATABASE_URL = os.environ.get('DATABASE_URL')
     with psycopg2.connect(DATABASE_URL, cursor_factory=psycopg2.extras.RealDictCursor) as conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT * FROM tickets WHERE ticket_id=%(ticket_id)s;", {"ticket_id": id_})
+            cur.execute("SELECT ticket_id, cast_member_name, o.order_number, ticket_number, s.showtime, on_roof, "
+                        "seating_group, tickets_in_order, \"cast\" FROM tickets INNER JOIN orders o on "
+                        "tickets.order_number = o.order_number INNER JOIN showtimes s on tickets.showtime = s.showtime "
+                        " WHERE ticket_id=%(ticket_id)s ORDER BY tickets.order_number, ticket_number;",
+                        {"ticket_id": id_})
             return flask.Response(json.dumps(dict(cur.fetchone()), default=str), mimetype="application/json")
 
 
