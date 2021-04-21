79984e4f61a13d84c7fb621dbfa5e158744386dc
Seth Peace <sethevanpeace@gmail.com>
2021-04-21 14:16:12 -0700

    fetches ticket info from server

--- static/py/index.py
+++ static/py/index.py
@@ -1,16 +1,23 @@
-from browser import window, document
-
-scanner = window.Instascan.Scanner.new({"video": document.getElementById('preview'),
-                                        "mirror": False})
-
-scanner.addListener('scan', lambda content: window.jQuery('#content-goes-here').html(content))
+from browser import window, document, ajax
 
 
-def startCamera(cameras):
+def start_camera(cameras):
     if len(cameras) > 0:
         scanner.start(cameras[0])
     else:
         window.jQuery('#content-goes-here').html('No cameras found.')
 
 
-window.Instascan.Camera.getCameras().then(startCamera).catch(lambda e: window.jQuery('#content-goes-here').html(e))
+def get_ticket(ticket_id):
+    def on_complete(request):
+        global content
+        content = request.text
+    content = ""
+    ajax.get(f'/internals/get_ticket/{ticket_id}', oncomplete=on_complete)
+    return content
+
+
+scanner = window.Instascan.Scanner.new({"video": document.getElementById('preview'), "mirror": False})
+scanner.addListener('scan', lambda content: window.jQuery('#content-goes-here').html(get_ticket(content)))
+
+window.Instascan.Camera.getCameras().then(start_camera).catch(lambda e: window.jQuery('#content-goes-here').html(e))
