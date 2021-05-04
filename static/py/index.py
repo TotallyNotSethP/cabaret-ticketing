466466417bd8ff38d51a8cd982857933f3d0d6ac
Seth Peace <sethevanpeace@gmail.com>
2021-05-04 14:02:34 -0700

    I HATE APPLE SO BAD

--- static/py/index.py
+++ static/py/index.py
@@ -34,7 +34,7 @@ def start_camera(cameras):
         else:
             scanner.start(cameras[0])
             document.getElementsByClassName("first-camera")[0].selected = True
-            window.jQuery('#cam-placeholder').remove()
+            window.jQuery('#cam-placeholder').hide()
     else:
         window.jQuery('#qr-code-info').html('No cameras found.')
 
